a36ae707bf0ec615dc1028ac8644122f5d5e4251
Stephen Sylvia <ssylvia@esri.com>
2013-08-20 14:56:20 -0400

    update api for ago release

--- javascript/map.js
+++ javascript/map.js
@@ -326,7 +326,7 @@ if (_timeProperties[index] != null) {
     });
 
     timeSlider.startup();
-
+    
     $("#timeSliderDiv > table > tbody > tr > td > span").hide();
     dojo.forEach(dojo.query(".timeControl"), function (qry) {
         dojo.style(qry, "display", "block");
